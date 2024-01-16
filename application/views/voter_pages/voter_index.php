d2080211bab7b07995655b2611123f20071b95c1
Njoku Okechukwu Val <bonsoirval@gmail.com>
2024-01-16 08:35:37 +0100

    polltest.sql added

--- application/views/voter_pages/voter_index.php
+++ application/views/voter_pages/voter_index.php
@@ -7,7 +7,7 @@
   <tbody>
     <?php if (count($elections) >= 1) { ?>
     <form method = 'POST' action="<?php echo base_url('index.php/voter_dashboard/vote'); ?>">
-    <?php for($index=0; $index < count($elections); $index++) {?>
+    <?php for($index=0; $index < count($elections); $index++) 
       <?php $candidates = get_candidate($elections[$index]['election_id']); ?>
       <?php //var_dump($candidates); ?>
       <tr>
